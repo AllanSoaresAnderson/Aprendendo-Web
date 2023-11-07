00642255c12e5575d37f82cf051cfb7d3905a307
Allan Soares Anderson <allansoaresanderson@gmail.com>
2023-11-07 19:15:46 -0300

    Angular project created

--- Bootcamp-Angular/Projetos-Angular/angular-blog/karma.conf.js
+++ Bootcamp-Angular/Projetos-Angular/angular-blog/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/life-cycle'),
+      dir: require('path').join(__dirname, './coverage/angular-blog'),
       subdir: '.',
       reporters: [
         { type: 'html' },
